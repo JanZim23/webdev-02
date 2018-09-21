90a668857f226a0092ca113aeb21c7fbe623a095
Jan Zimmermann <janzim23@gmail.com>
2018-09-20 20:41:07 -0400

    First

--- lib/practice.ex
+++ lib/practice.ex
@@ -18,8 +18,11 @@ defmodule Practice do
 
   def factor(x) do
     # Maybe delegate this too.
-    [1,2,x]
+    Practice.Factor.factor(x);
   end
 
-  # TODO: Add a palindrome? function.
+  def paindrome?(p) do
+      p == String.reverse(p)
+  end
+
 end
