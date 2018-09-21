261f839f5e5a61488562f1d64c943d38630558c8
Jan Zimmermann <janzim23@gmail.com>
2018-09-20 21:11:40 -0400

    b

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -1,19 +1,5 @@
 defmodule Practice.Factor do
   def factor(number) do
-    # This should handle +,-,*,/ with order of operations,
-    # but doesn't need to handle parens.
-    expr
-    |> String.split(~r/\s+/)
-    |> hd
-    |> parse_float
-    |> :math.sqrt()
-
-    # Hint:
-    # expr
-    # |> split
-    # |> tag_tokens  (e.g. [+, 1] => [{:op, "+"}, {:num, 1.0}]
-    # |> convert to postfix
-    # |> reverse to prefix
-    # |> evaluate as a stack calculator using pattern matching
+    
   end
 end
